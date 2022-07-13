3936626c6d43d35fc733b942ebe03b64547180dc
namyoungjun96 <ehdgornltls@naver.com>
2022-07-14 01:16:19 +0900

    bootstrap and background image

--- js/background.js
+++ js/background.js
@@ -1,7 +1,10 @@
 const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
-const bgImage = document.createElement("img");
+// const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`;
+// bgImage.id = "bgImage";
+// bgImage.src = `img/${chosenImage}`;
 
-document.body.appendChild(bgImage);
+// document.body.appendChild(bgImage);
+
+document.body.style.backgroundImage = `url(img/${chosenImage})`;
